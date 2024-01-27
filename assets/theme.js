1927497ae3e7ae7590f464122d4230d818ed947b
Timwfowler <47511473+timwf@users.noreply.github.com>
2024-01-27 12:13:22 +0700

    typography

--- assets/theme.js
+++ assets/theme.js
@@ -3359,143 +3359,143 @@ lazySizesConfig.expFactor = 4;
   //   - .product-video-mp4-sound video player element (cloned into modal)
   //     - see media.liquid for example of this
   theme.videoModal = function() {
-    var youtubePlayer;
-    var vimeoPlayer;
+    // var youtubePlayer;
+    // var vimeoPlayer;
   
-    var videoHolderId = 'VideoHolder';
-    var selectors = {
-      youtube: 'a[href*="youtube.com/watch"], a[href*="youtu.be/"]',
-      vimeo: 'a[href*="player.vimeo.com/player/"], a[href*="vimeo.com/"]',
-      mp4Trigger: '.product-video-trigger--mp4',
-      mp4Player: '.product-video-mp4-sound'
-    };
+    // var videoHolderId = 'VideoHolder';
+    // var selectors = {
+    //   youtube: 'a[href*="youtube.com/watch"], a[href*="youtu.be/"]',
+    //   vimeo: 'a[href*="player.vimeo.com/player/"], a[href*="vimeo.com/"]',
+    //   mp4Trigger: '.product-video-trigger--mp4',
+    //   mp4Player: '.product-video-mp4-sound'
+    // };
   
-    var youtubeTriggers = document.querySelectorAll(selectors.youtube);
-    var vimeoTriggers = document.querySelectorAll(selectors.vimeo);
-    var mp4Triggers = document.querySelectorAll(selectors.mp4Trigger);
+    // var youtubeTriggers = document.querySelectorAll(selectors.youtube);
+    // var vimeoTriggers = document.querySelectorAll(selectors.vimeo);
+    // var mp4Triggers = document.querySelectorAll(selectors.mp4Trigger);
   
-    if (!youtubeTriggers.length && !vimeoTriggers.length && !mp4Triggers.length) {
-      return;
-    }
+    // if (!youtubeTriggers.length && !vimeoTriggers.length && !mp4Triggers.length) {
+    //   return;
+    // }
   
-    var videoHolderDiv = document.getElementById(videoHolderId);
+    // var videoHolderDiv = document.getElementById(videoHolderId);
   
-    if (youtubeTriggers.length) {
-      theme.LibraryLoader.load('youtubeSdk');
-    }
+    // if (youtubeTriggers.length) {
+    //   theme.LibraryLoader.load('youtubeSdk');
+    // }
   
-    if (vimeoTriggers.length) {
-      theme.LibraryLoader.load('vimeo', window.vimeoApiReady);
-    }
+    // if (vimeoTriggers.length) {
+    //   theme.LibraryLoader.load('vimeo', window.vimeoApiReady);
+    // }
   
-    var modal = new theme.Modals('VideoModal', 'video-modal', {
-      closeOffContentClick: true,
-      bodyOpenClass: ['modal-open', 'video-modal-open'],
-      solid: true
-    });
+    // var modal = new theme.Modals('VideoModal', 'video-modal', {
+    //   closeOffContentClick: true,
+    //   bodyOpenClass: ['modal-open', 'video-modal-open'],
+    //   solid: true
+    // });
   
-    youtubeTriggers.forEach(btn => {
-      btn.addEventListener('click', triggerYouTubeModal);
-    });
+    // youtubeTriggers.forEach(btn => {
+    //   btn.addEventListener('click', triggerYouTubeModal);
+    // });
   
-    vimeoTriggers.forEach(btn => {
-      btn.addEventListener('click', triggerVimeoModal);
-    });
+    // vimeoTriggers.forEach(btn => {
+    //   btn.addEventListener('click', triggerVimeoModal);
+    // });
   
-    mp4Triggers.forEach(btn => {
-      btn.addEventListener('click', triggerMp4Modal);
-    });
+    // mp4Triggers.forEach(btn => {
+    //   btn.addEventListener('click', triggerMp4Modal);
+    // });
   
-    document.addEventListener('modalClose.VideoModal', closeVideoModal);
+    // document.addEventListener('modalClose.VideoModal', closeVideoModal);
   
-    function triggerYouTubeModal(evt) {
-      // If not already loaded, treat as normal link
-      if (!theme.config.youTubeReady) {
-        return;
-      }
+    // function triggerYouTubeModal(evt) {
+    //   // If not already loaded, treat as normal link
+    //   if (!theme.config.youTubeReady) {
+    //     return;
+    //   }
   
-      evt.preventDefault();
-      emptyVideoHolder();
+    //   evt.preventDefault();
+    //   emptyVideoHolder();
   
-      modal.open(evt);
+    //   modal.open(evt);
   
-      var videoId = getYoutubeVideoId(evt.currentTarget.getAttribute('href'));
-      youtubePlayer = new theme.YouTube(
-        videoHolderId,
-        {
-          videoId: videoId,
-          style: 'sound',
-          events: {
-            onReady: onYoutubeReady
-          }
-        }
-      );
-    }
+    //   var videoId = getYoutubeVideoId(evt.currentTarget.getAttribute('href'));
+    //   youtubePlayer = new theme.YouTube(
+    //     videoHolderId,
+    //     {
+    //       videoId: videoId,
+    //       style: 'sound',
+    //       events: {
+    //         onReady: onYoutubeReady
+    //       }
+    //     }
+    //   );
+    // }
   
-    function triggerVimeoModal(evt) {
-      // If not already loaded, treat as normal link
-      if (!theme.config.vimeoReady) {
-        return;
-      }
+    // function triggerVimeoModal(evt) {
+    //   // If not already loaded, treat as normal link
+    //   if (!theme.config.vimeoReady) {
+    //     return;
+    //   }
   
-      evt.preventDefault();
-      emptyVideoHolder();
+    //   evt.preventDefault();
+    //   emptyVideoHolder();
   
-      modal.open(evt);
+    //   modal.open(evt);
   
-      var videoId = evt.currentTarget.dataset.videoId;
-      var videoLoop = evt.currentTarget.dataset.videoLoop;
-      vimeoPlayer = new theme.VimeoPlayer(
-        videoHolderId,
-        videoId,
-        {
-          style: 'sound',
-          loop: videoLoop,
-        }
-      );
-    }
+    //   var videoId = evt.currentTarget.dataset.videoId;
+    //   var videoLoop = evt.currentTarget.dataset.videoLoop;
+    //   vimeoPlayer = new theme.VimeoPlayer(
+    //     videoHolderId,
+    //     videoId,
+    //     {
+    //       style: 'sound',
+    //       loop: videoLoop,
+    //     }
+    //   );
+    // }
   
-    function triggerMp4Modal(evt) {
-      emptyVideoHolder();
+    // function triggerMp4Modal(evt) {
+    //   emptyVideoHolder();
   
-      var el = evt.currentTarget;
-      var player = el.parentNode.querySelector(selectors.mp4Player);
+    //   var el = evt.currentTarget;
+    //   var player = el.parentNode.querySelector(selectors.mp4Player);
   
-      // Clone video element and place it in the modal
-      var playerClone = player.cloneNode(true);
-      playerClone.classList.remove('hide');
+    //   // Clone video element and place it in the modal
+    //   var playerClone = player.cloneNode(true);
+    //   playerClone.classList.remove('hide');
   
-      videoHolderDiv.append(playerClone);
-      modal.open(evt);
+    //   videoHolderDiv.append(playerClone);
+    //   modal.open(evt);
   
-      // Play new video element
-      videoHolderDiv.querySelector('video').play();
-    }
+    //   // Play new video element
+    //   videoHolderDiv.querySelector('video').play();
+    // }
   
-    function onYoutubeReady(evt) {
-      evt.target.unMute();
-      evt.target.playVideo();
-    }
+    // function onYoutubeReady(evt) {
+    //   evt.target.unMute();
+    //   evt.target.playVideo();
+    // }
   
-    function getYoutubeVideoId(url) {
-      var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
-      var match = url.match(regExp);
-      return (match&&match[7].length==11)? match[7] : false;
-    }
+    // function getYoutubeVideoId(url) {
+    //   var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+    //   var match = url.match(regExp);
+    //   return (match&&match[7].length==11)? match[7] : false;
+    // }
   
-    function emptyVideoHolder() {
-      videoHolderDiv.innerHTML = '';
-    }
+    // function emptyVideoHolder() {
+    //   videoHolderDiv.innerHTML = '';
+    // }
   
-    function closeVideoModal() {
-      if (youtubePlayer && typeof youtubePlayer.destroy === 'function') {
-        youtubePlayer.destroy();
-      } else if (vimeoPlayer && typeof vimeoPlayer.destroy === 'function') {
-        vimeoPlayer.destroy();
-      } else {
-        emptyVideoHolder();
-      }
-    }
+    // function closeVideoModal() {
+    //   if (youtubePlayer && typeof youtubePlayer.destroy === 'function') {
+    //     youtubePlayer.destroy();
+    //   } else if (vimeoPlayer && typeof vimeoPlayer.destroy === 'function') {
+    //     vimeoPlayer.destroy();
+    //   } else {
+    //     emptyVideoHolder();
+    //   }
+    // }
   };
   
   
@@ -6428,13 +6428,13 @@ lazySizesConfig.expFactor = 4;
       },
   
       initYoutubeVideo: function(videoId) {
-        this.videoObject = new theme.YouTube(
-          'YouTubeVideo-' + this.sectionId,
-          {
-            videoId: videoId,
-            videoParent: selectors.videoParent
-          }
-        );
+        // this.videoObject = new theme.YouTube(
+        //   'YouTubeVideo-' + this.sectionId,
+        //   {
+        //     videoId: videoId,
+        //     videoParent: selectors.videoParent
+        //   }
+        // );
       },
   
       initVimeoVideo: function(videoId) {
@@ -8046,20 +8046,20 @@ lazySizesConfig.expFactor = 4;
       },
   
       initYoutubeVideo: function(div) {
-        videoObjects[div.id] = new theme.YouTube(
-          div.id,
-          {
-            videoId: div.dataset.videoId,
-            videoParent: selectors.videoParent,
-            autoplay: false, // will handle this in callback
-            style: div.dataset.videoStyle,
-            loop: div.dataset.videoLoop,
-            events: {
-              onReady: this.youtubePlayerReady.bind(this),
-              onStateChange: this.youtubePlayerStateChange.bind(this)
-            }
-          }
-        );
+        // videoObjects[div.id] = new theme.YouTube(
+        //   div.id,
+        //   {
+        //     videoId: div.dataset.videoId,
+        //     videoParent: selectors.videoParent,
+        //     autoplay: false, // will handle this in callback
+        //     style: div.dataset.videoStyle,
+        //     loop: div.dataset.videoLoop,
+        //     events: {
+        //       onReady: this.youtubePlayerReady.bind(this),
+        //       onStateChange: this.youtubePlayerStateChange.bind(this)
+        //     }
+        //   }
+        // );
       },
   
       initVimeoVideo: function(div) {
@@ -8143,15 +8143,15 @@ lazySizesConfig.expFactor = 4;
       },
   
       stopVideos: function() {
-        for (var [id, vid] of Object.entries(videoObjects)) {
-          if (vid.videoPlayer) {
-            if (typeof vid.videoPlayer.stopVideo === 'function') {
-              vid.videoPlayer.stopVideo(); // YouTube player
-            }
-          } else if (vid.type === 'mp4') {
-            this.stopMp4Video(vid.id); // MP4 player
-          }
-        }
+        // for (var [id, vid] of Object.entries(videoObjects)) {
+        //   if (vid.videoPlayer) {
+        //     if (typeof vid.videoPlayer.stopVideo === 'function') {
+        //       vid.videoPlayer.stopVideo(); // YouTube player
+        //     }
+        //   } else if (vid.type === 'mp4') {
+        //     this.stopMp4Video(vid.id); // MP4 player
+        //   }
+        // }
       },
   
       _getVideoType: function(video) {
